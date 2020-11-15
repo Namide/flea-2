b99d06281dbc0160f1ca7a0d010dfaec2f500f57
Namide <damien.git@gmail.com>
2020-11-15 20:48:19 +0100

    Test env

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ const pageFiles = glob.sync('src/content/**/*.md')
 let pagesRequire = ''
 const htmlWebpackPlugins = pageFiles
   .map(fileName => {
+
     const permalink = fileName.replace('src/content/', '/').replace('.md', '')
     const meta = Object.assign({ permalink }, CONFIG.default, getYaml(fileName))
     const filesrc = fileName.replace('src/content/', '')
@@ -34,73 +35,103 @@ const htmlWebpackPlugins = pageFiles
 
     PAGES.push(options)
     
+    console.log('Page: /' + meta.permalink)
+    
     return new HtmlWebpackPlugin(options)
   })
 
 fs.writeFileSync('src/theme/_pages.js', 'const list = {}\n' + pagesRequire + 'module.exports = list')
+console.log('Page count:', pageFiles.length, '/n')
 
 // Webpack
-module.exports = {
-  mode: 'production',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: 8080,
-    historyApiFallback: true,
-    hot: true,
-    open: true
-  },
-  entry: {
-    "main.js": "./src/theme/script/main.js",
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: "assets/script/[name]",
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'assets/[path][name].[ext]',
-          context: 'src'
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-                name: "assets/style/[name].[ext]",
-            },
-          },
-          "extract-loader",
-          'css-loader'
-        ],
-      },
-      {
-        test: /\.ejs$/,
-        use: [
-          {
-            loader: 'ejs-loader',
-            options: {
-              esModule: false
-            }
-          },
-        ],
-      }
-    ]
-  },
+module.exports = env => {
 
-  plugins: [
-    ...htmlWebpackPlugins,
-    new CleanWebpackPlugin(),
-    new CopyPlugin({
-      patterns: [
-        { from: "src/content/static", to: "" }
-      ],
-    }),
-  ]
+  const isDev = !!env.development
+  const isProd = !!env.production
+
+  return {
+
+    // mode: 'production',
+    mode: isProd ? "production" : "development",
+    watch: isDev,
+  
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      port: 8080,
+      historyApiFallback: true,
+      hot: isDev,
+      open: true
+    },
+
+    entry: {
+      "main.js": "./src/theme/script/main.js",
+    },
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: "assets/script/[name]",
+      publicPath: '/'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(svg|png|jpe?g|gif)$/i,
+          loader: 'file-loader',
+          options: {
+            name: 'assets/[path][name].[ext]',
+            context: 'src'
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                  name: "assets/style/[name].[ext]",
+              },
+            },
+            "extract-loader",
+            'css-loader'
+          ],
+        },
+        {
+          test: /\.scss$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                  name: "assets/style/[name].[ext]",
+              },
+            },
+            "extract-loader",
+            'css-loader',
+            'sass-loader'
+          ],
+        },
+        {
+          test: /\.ejs$/,
+          use: [
+            {
+              loader: 'ejs-loader',
+              options: {
+                esModule: false
+              }
+            },
+          ],
+        }
+      ]
+    },
+  
+    plugins: [
+      ...htmlWebpackPlugins,
+      new CleanWebpackPlugin(),
+      new CopyPlugin({
+        patterns: [
+          { from: "src/content/static", to: "" }
+        ],
+      }),
+    ]
+  }
 }
